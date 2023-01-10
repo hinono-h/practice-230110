f60e2d6b1295b6d270c4c82a2871a308c95d9c82
hinono-h <yudayudayou@gmail.com>
2023-01-10 17:20:15 +0900

    Practice for rust.
    Co-authored-by: Akiba, S. <anosatsuk124@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use symphonia::core::io::MediaSourceStream;
+use symphonia::core::probe::Hint;
+use symphonia::core::{formats, meta};
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let path = args.get(1).expect("Not Found any files.");
-    
-    println!("File name: {args}");
+
+    let file = std::fs::File::open(&path).expect("Cannot open a file.");
+
+    let media_src_stream = MediaSourceStream::new(Box::new(file), Default::default());
+
+    let mut hint = Hint::new();
+    hint.with_extension("wav");
+
+    let meta_options: meta::MetadataOptions = Default::default();
+    let fmt_options: formats::FormatOptions = Default::default();
+
+    let probe = symphonia::default::get_probe()
+        .format(&hint, media_src_stream, &fmt_options, &meta_options)
+        .expect("Unsupported format error.");
+
+    let mut format = probe.format;
 }
